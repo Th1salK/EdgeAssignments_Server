4b58e917e71ef91f8c44c0771f9f80610027fcd7
Thisal Kokuhennadi <thisalkh22@gmail.com>
2026-01-14 22:25:41 +0530

    Initial Commit

---
+++ EdgeAssignments.API/Controllers/BaseController.cs
@@ -0,0 +1,161 @@
+﻿using AutoMapper;
+using EdgeFlow.API.Dtos.Get;
+using EdgeFlow.API.Dtos.Post;
+using EdgeFlow.Core.Domain.Storage;
+using EdgeFlow.Core.Domain.Storage.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Formatter;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Query.Wrapper;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using SimplePatch;
+using System.Web;
+
+namespace EdgeFlow.API.Controllers
+{
+    public class BaseController<E, R, W> : ODataController where E : BaseEntity, new() where R : BaseGetDto, new() where W : BasePostDto, new()
+    {
+        private readonly IBaseRepository<E> entityRepo;
+        private readonly IMapper mapper;
+        private readonly ILogger<E> logger;
+
+        public BaseController(IBaseRepository<E> entityRepo, IMapper mapper, ILogger<E> _logger)
+        {
+            this.entityRepo = entityRepo;
+            this.mapper = mapper;
+            logger = _logger;
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<R>> TextSearch(string searchText, string searchFields)
+        {
+            var searchFieldsList = HttpUtility.UrlDecode(searchFields).Split(",").ToList();
+            var res = await entityRepo.TextSearchAsync(searchText, searchFieldsList);
+            var response = mapper.Map<List<R>>(res);
+            return Ok(response);
+        }
+        [HttpGet("{id}")]
+        public virtual async Task<IActionResult> GetSingle(string id)
+        {
+            var result = await entityRepo.GetByIdAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            var mappedResult = mapper.Map<R>(result);
+            return Ok(mappedResult);
+        }
+        [HttpGet]
+        [EnableQuery(MaxAnyAllExpressionDepth =20, MaxNodeCount =300)]
+        public virtual async Task<ActionResult<List<R>>> Get(ODataQueryOptions<R> oDataQueryOptions)
+        {
+            var searchText = oDataQueryOptions.Search?.RawValue;
+            var cvList = new List<E>();
+            if (searchText != null)
+            {
+               string typeName = typeof(E).Name;
+                switch(typeName)
+                {
+                    case "Cv":
+                        cvList = await entityRepo.TextSearchAsync(searchText, new List<string> {
+                            "Profile.FirstName", 
+                            "Profile.LastName", 
+                            "Profile.Email",
+                            "Profile.ShortIntro",
+                            "Profile.LongIntro",
+                            "Industries",
+                            "ExpAreas",
+                            "ToolsAndMethods.ToolsWithScale.Name",
+                            "Certificates.Name",
+                            "Certificates.Provider",
+                            "Educations.Name",
+                            "Educations.Provider",
+                            "Courses.Name",
+                            "Courses.Provider",
+                            "Languages.Name",
+                            "Employments.Name",
+                            "Engagements.Client",
+                            "Engagements.Role",
+                            "Engagements.Project",
+                            "Engagements.ShortIntro",
+                            "Engagements.LongIntro"
+
+
+                        });
+
+                        break;
+
+                    default:
+                        cvList = await entityRepo.GetAllAsync();
+                        break;
+                }
+
+                    
+            }
+            else
+            {
+                cvList = await entityRepo.GetAllAsync();
+            }
+         
+            var mappedResult = mapper.Map<List<R>>(cvList);
+            return Ok(mappedResult);
+        }
+
+        [HttpPost]
+        public virtual async Task<ActionResult<R>> Post(W data)
+        {
+            var userId = HttpContext.Request.Headers["X-UserId"].FirstOrDefault();
+
+            var entity = mapper.Map<E>(data);
+            entity.CreatedBy = userId;
+            entity.LastUpdated = DateTime.Now;
+            var res = await entityRepo.AddAsync(entity);
+            var response = mapper.Map<R>(res);
+            return Ok(response);
+        }
+        [HttpPut("{id}")]
+        public virtual async Task<ActionResult<R>> Put(string id, W data)
+        {
+            var userId = HttpContext.Request.Headers["X-UserId"].FirstOrDefault();
+            var entity = mapper.Map<E>(data);
+            entity.Id = id;
+            entity.UpdatedBy = userId;
+            entity.LastUpdated = DateTime.Now;
+            var res = await entityRepo.UpdateAsync(entity);
+            var response = mapper.Map<R>(res);
+            return Ok(response);
+        }
+        [HttpDelete("{id}")]
+        public virtual async Task<ActionResult<R>> Delete(string id)
+        {
+            await entityRepo.DeleteAsync(id);
+            return Ok();
+        }
+
+        [HttpPatch("{id}")]
+        public virtual async Task<ActionResult<R>> Patch(string id, [FromBody] JsonPatchDocument<W> data)
+        {
+
+            var userId = HttpContext.Request.Headers["X-UserId"].FirstOrDefault();
+
+            var entity = await entityRepo.GetByIdAsync(id);
+            entity.UpdatedBy = userId;
+            var mappedData = mapper.Map<W>(entity);
+            data.ApplyTo(mappedData, ModelState);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var remapp = mapper.Map<E>(mappedData);
+
+            remapp.LastUpdated = DateTime.Now;
+            var res = await entityRepo.UpdateAsync(remapp);
+            var response = mapper.Map<R>(res);
+            return Ok(response);
+        }
+
+    }
+}
---
+++ EdgeAssignments.Infrastructure.Persistence/MongoRepository.cs
@@ -0,0 +1,229 @@
+﻿using EdgeFlow.Core.Domain.Storage;
+using EdgeFlow.Core.Domain.Storage.Configs;
+using EdgeFlow.Core.Domain.Storage.Entities;
+using Microsoft.AspNetCore.Http;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using SharpCompress.Common;
+using System.Linq.Expressions;
+
+namespace EdgeFlow.Infrastructure.Persistence
+{
+    public class MongoRepository<T> : IBaseRepository<T> where T : BaseEntity
+    {
+        protected readonly IMongoClient client;
+
+        protected readonly IMongoCollection<T> Collection;
+        protected readonly IMongoCollection<BsonDocument> bsonCollection;
+        private readonly ApplicationSettings settings;
+
+        public MongoRepository(ApplicationSettings settings, IHttpContextAccessor httpContentAccessor)
+        {
+            client = new MongoClient(settings.ConnectionString);
+            var collectionName = typeof(T).Name;
+            var tenant = httpContentAccessor?.HttpContext?.Request?.Headers["X-Tenant"].FirstOrDefault();
+
+            if(tenant!=null)
+            {
+                Collection = client
+                    .GetDatabase(tenant)
+                    .GetCollection<T>(collectionName.ToLower());
+
+                bsonCollection = client
+                   .GetDatabase(tenant)
+                   .GetCollection<BsonDocument>(collectionName.ToLower());
+            }
+            else
+            {
+                Collection = client
+                    .GetDatabase(settings.DatabaseName)
+                    .GetCollection<T>(collectionName.ToLower());
+
+                bsonCollection = client
+                   .GetDatabase(settings.DatabaseName)
+                   .GetCollection<BsonDocument>(collectionName.ToLower());
+            }
+            
+            this.settings = settings;
+        }
+
+        public async Task<List<T>> TextSearchAsync(string searchText, List<string> fieldsList)
+        {
+            var searchTextFilter = new List<FilterDefinition<T>>();
+            foreach (var field in fieldsList)
+            {
+                searchTextFilter.Add(Builders<T>.Filter.Regex(field, new BsonRegularExpression(searchText, "i")));
+            }
+            var filter = Builders<T>.Filter.Or(searchTextFilter);
+            return await Collection.Aggregate()
+                .Match(filter)
+                .ToListAsync();
+        }
+
+        public async Task<T> AddAsync(T obj)
+        {
+
+            var document = obj.ToBsonDocument();
+            document["_id"] = ObjectId.Empty;
+            await bsonCollection.InsertOneAsync(document);
+            obj.Id = document["_id"].ToString();
+
+            return obj;
+
+        }
+
+        public async Task<T> AddWithIdAsync(T obj)
+        {
+            var document = obj.ToBsonDocument();
+            await bsonCollection.InsertOneAsync(document);
+            obj.Id = document["_id"].ToString();
+            return obj;
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await Collection.DeleteOneAsync(x => x.Id == id);
+        }
+
+        public async Task DeleteAsync(Expression<Func<T, bool>> predicate)
+        {
+            await Collection.DeleteManyAsync(predicate);
+        }
+
+        public async Task<List<T>> GetAllAsync(string tenant=null)
+        {
+            if(tenant!=null)
+            {
+                return await client.GetDatabase(tenant)
+                    .GetCollection<T>(typeof(T).Name.ToLower())
+                    .AsQueryable().ToListAsync();
+            }
+            else
+            {
+                return await Collection.AsQueryable().ToListAsync();
+            }
+           
+        }
+
+        public async Task<T> GetByIdAsync(string id)
+        {
+            return await Collection.Find(Builders<T>.Filter.Eq(x => x.Id, id))
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<List<T>> GetByIdsAsync(List<string> id,string tenant = null)
+        {
+            if(tenant!=null)
+            {
+                return await client.GetDatabase(tenant)
+                    .GetCollection<T>(typeof(T).Name.ToLower())
+                    .AsQueryable().Where(x => id.Contains(x.Id)).ToListAsync();
+            }
+            else
+            {
+                return await Collection.AsQueryable().Where(x => id.Contains(x.Id)).ToListAsync();
+            }
+         
+        }
+
+
+        public async Task PutAllAsync(IEnumerable<T> entities)
+        {
+            var filterBuilder = Builders<T>.Filter;
+            var newItems = entities.Select(item => new ReplaceOneModel<T>(filterBuilder.Where(x => x.Id == item.Id), item));
+
+            await Collection.BulkWriteAsync(newItems);
+        }
+
+        public async Task<List<T>> SearchAsync(Expression<Func<T, bool>> predicate, string tenant = null)
+        {
+            if (tenant != null)
+            {
+                var result= await client.GetDatabase(tenant).GetCollection<T>(typeof(T).Name.ToLower()).FindAsync(predicate);
+                return result.ToList();
+
+            }
+            else
+            {
+              
+                var result = await Collection.FindAsync(predicate);
+                return result.ToList();
+            }
+        }
+
+        public async Task<T> UpdateAsync(T obj, Expression<Func<T, bool>>? predicate = null)
+        {
+            if (predicate == null)
+            {
+                predicate = (p) => p.Id == obj.Id;
+            }
+            await Collection.ReplaceOneAsync(predicate, obj);
+            return obj;
+        }
+
+        public async Task AddAllAsync(IEnumerable<T> entities)
+		{
+			var documents = new List<BsonDocument>();
+
+			foreach (var entity in entities)
+			{
+                // Generate a new ObjectId for every entity
+                if (String.IsNullOrEmpty(entity.Id))
+                {
+                   var objectId = ObjectId.GenerateNewId();
+                   entity.Id = objectId.ToString();
+                }
+
+				// Convert to BsonDocument and set the _id
+				var doc = entity.ToBsonDocument();
+				doc["_id"] = ObjectId.Parse(entity.Id);
+
+				documents.Add(doc);
+			}
+
+			await bsonCollection.InsertManyAsync(documents);
+		}
+
+        public async Task UpdateAllAsync(IEnumerable<T> entities)
+        {
+            if (entities == null)
+                return;
+
+            var bulkOps = new List<WriteModel<T>>();
+
+            foreach (var entity in entities)
+            {
+                if (string.IsNullOrEmpty(entity.Id))
+                {
+                    throw new ArgumentException("Entity Id cannot be null or empty for update.");
+                }
+
+                var doc = entity.ToBsonDocument();
+                doc["_id"] = ObjectId.Parse(entity.Id);
+
+                var filter = Builders<T>.Filter.Eq(e => e.Id, entity.Id);
+
+                var replaceOp = new ReplaceOneModel<T>(filter, entity)
+                {
+                    IsUpsert = false // set true if you want to insert if not exists
+                };
+
+                bulkOps.Add(replaceOp);
+            }
+
+            if (bulkOps.Count > 0)
+            {
+                await Collection.BulkWriteAsync(bulkOps);
+            }
+        }
+
+
+        public Task<int> CountAsync()
+        {
+            return Collection.AsQueryable().CountAsync();
+        }
+
+
+    }
+}
