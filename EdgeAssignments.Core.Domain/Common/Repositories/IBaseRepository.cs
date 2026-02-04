1e9e9229c544895a2e739870ab4a7dc335c447d0
Vineth Ranathunga <vineth.ranathunga@gmail.com>
2026-02-04 20:36:43 +0530

    Add initial domain and repository classes for EdgeAssignments

---
+++ EdgeAssignments.Core.Domain/Common/Repositories/IBaseRepository.cs
@@ -0,0 +1,22 @@
+using EdgeAssignments.Core.Domain.Storage.Entities;
+using System.Linq.Expressions;
+
+namespace EdgeAssignments.Core.Domain.Common.Repositories;
+
+public interface IBaseRepository<T> where T : BaseEntity
+{
+    Task<List<T>> TextSearchAsync(string searchText, List<string> fieldsList);
+    Task<T> AddAsync(T obj);
+    Task<T> AddWithIdAsync(T obj);
+    Task DeleteAsync(string id);
+    Task DeleteAsync(Expression<Func<T, bool>> predicate);
+    Task<List<T>> GetAllAsync(string? tenant = null);
+    Task<T> GetByIdAsync(string id);
+    Task<List<T>> GetByIdsAsync(List<string> id, string? tenant = null);
+    Task PutAllAsync(IEnumerable<T> entities);
+    Task<List<T>> SearchAsync(Expression<Func<T, bool>> predicate, string? tenant = null);
+    Task<T> UpdateAsync(T obj, Expression<Func<T, bool>>? predicate = null);
+    Task AddAllAsync(IEnumerable<T> entities);
+    Task UpdateAllAsync(IEnumerable<T> entities);
+    Task<int> CountAsync();
+}
